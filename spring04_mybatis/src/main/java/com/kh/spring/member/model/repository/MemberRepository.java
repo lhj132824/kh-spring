37d4494ed023eb9f248dbcc2521c330db9686b7f
lhj132824 <lhj132824@gmail.com>
2021-10-15 15:17:37 +0900

    20211015

---
+++ spring04_mybatis/src/main/java/com/kh/spring/member/model/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.kh.spring.member.model.repository;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+	
+	@Autowired
+	private SqlSessionTemplate session;
+	
+	public String selectPasswordByUserId(String userId) {
+		return session.selectOne("com.kh.spring.member.mybatis.MybatisMapper.selectPasswordByUserId", userId);
+	}
+}
